1a34fe2dbe2ef044669e16ff88bffea47a5b1f90
Per Eriksson <per_hegg@hotmail.com>
2019-03-05 14:42:50 +0100

    Test environment setup and working correctly

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -9,7 +9,8 @@ module.exports = {
             '--ignore-certificate-errors',
             "--disable-popup-blocking",
             "--disable-infobars",
-            '--disable-web-security']
+            '--disable-web-security',
+            "--shm-size=1gb"]
     },
     browserContext: 'default',
 
